7cfea7f9d4c77a207e863e9e9c3dd24a4f73380d
michellehalim <m.chellehlm@gmail.com>
2025-12-09 03:55:00 +0800

    Revert "added new metadata"
    This reverts commit 84d2c4d73ad0d06e8e457ddf78ef9d1d0c38de29.

--- root/app/functions/fetch_metadata.py
+++ root/app/functions/fetch_metadata.py
@@ -179,8 +179,6 @@ def fetch_ifc_metadata(token: str):
                     large_building_key = key
                     break
 
-            host = ifc.get("IfcContainedInHost")
-
             filtered.append({
                 "name": item.get("name"),
                 "ifcAttributes": {
@@ -189,7 +187,6 @@ def fetch_ifc_metadata(token: str):
                     "IfcClass": ifc.get("IfcClass"),
                     "IfcPropertySetList": ifc.get("IfcPropertySetList"),
                     "IfcSpatialContainer": ifc.get("IfcSpatialContainer"),
-                    "IfcContainedInHost": host,
                 },
                 # Only keep the classifier ID, not its content
                 "classificationId": large_building_key
